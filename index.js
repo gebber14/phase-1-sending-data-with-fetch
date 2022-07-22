a7287ef1ac02ed82b9617abb6f90016232ffebba
Jason Rothgeb <jrothgeb@gmail.com>
2022-07-22 14:05:17 -0500

    commit tree 4b3e2c8bba6d31e2e374f70c67b8d4735820e713

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarok!");
+//         console.log(error.message);
+//     })
+
+
+function submitData(userName,userEmail)  {
+    const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    
+    body: JSON.stringify( {
+        name: userName,
+        email: userEmail,
+    }),
+    }
+    
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object.id;
+    })
+    .catch(function(error) {
+        alert("Unauthorized Access");
+        document.body.innerHTML = error.message;
+    })
+}
+
+
+
+ 
